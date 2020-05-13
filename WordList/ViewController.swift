6431fcba2afb481ad480a09b4701e242a88ebfd8
maya <amacrk@i.softbank.jp>
2020-05-13 20:47:36 +0900

    Initial Commit

---
+++ WordList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordList
+//
+//  Created by maya on 2020/05/13.
+//  Copyright © 2020 maya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
